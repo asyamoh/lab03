278453a9d8d5a602700eb51dee7ab5e449791c88
asiamohammad <>
2021-05-05 12:12:48 +0000

    jsss

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 alert('welcome')
 
-var userCar = prompt('Whats your favourite skincare  cleanser or serum??')
+var userChoice = prompt('Whats your favourite skincare  cleanser or serum??')
 
-if(userCar === 'serum'){
+if(userChoice === 'serum'){
   document.write('<img src="https://www.annahar.com/Library/Images/Uploaded%20Images/2020/10/28/Fadia/8-La-Roche-Posay-Effaclar-Serum.jpg">')
-}else if(userCar === 'cleanser'){
+}else if(userChoice === 'cleanser'){
   document.write('<img src="https://www.kiehls.ae/dw/image/v2/AAQP_PRD/on/demandware.static/-/Sites-kie-master-catalog/default/dwe1be2851/Products/Skincare/Centella_FacialCleanser250ml.jpg?sw=465&sh=390&sm=fit&q=70"?fit=around%7C248:139.5">')
 }else {
   alert('okay you should only pick serum or cleanser')
@@ -12,15 +12,15 @@ if(userCar === 'serum'){
 
 
 
-var x = 5
-var firstName = 'Asia '
-alert(firstName)
+var firstName = prompt('what is your name?? ') 
 
-var lastName = "Nazif"
-var myDad = "Mohammad"
-console.log(firstName + myDad+lastName)
-alert(firstName+lastName)
+alert( firstName )
+
+var welcoming = "good morning Beautiful"
+console.log(firstName + welcoming)
+alert(firstName + welcoming)
  
 
 confirm("gooddayy!");
+
 
